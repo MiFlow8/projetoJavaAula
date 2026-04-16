92ccc9f8ab0c8ef68d05490bd83230813b60061d
MiFlow8 <milenafernandes829@gmail.com>
2026-04-16 17:31:04 -0300

    Meu primeiro projetinho em Java

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {	// método principal que executa o código 
+	
+		
+		
+		System.out.print("Hello World");	// identico ao escreva 
+
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,29 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		// Variaveis
+		int idade = 25;	
+		String nome = "Lilian";
+		Float graus = 20.0f;	// float => ponto flutuante
+		boolean resposta;      // boolean => logicio true/false
+		
+		// Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.println(nome);
+		System.out.println(QUANTIDADE);
+		System.out.println(graus);
+		
+		
+		
+		
+		
+
+	}
+
+}
